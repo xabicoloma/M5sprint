ccd430e877d8d6eb6629b69f1194e8e7657784c3
xabicoloma <xabi165@gmail.com>
2023-07-10 20:15:52 -0400

    consultas

--- M5sprint.sql
+++ M5sprint.sql
@@ -44,3 +44,38 @@ values
 	(default,"Commander", "Shepard", 50, "csherpard@example.com",6),
 	(default,"Ezio", "Auditore", 50, "eauditore@example.com",18),
 	(default,"Arthur", "Morgan", 36, "amorgan@example.com",12);
+    
+insert into registros
+values
+	(default, 1, 5, "2023-04-12", 7),
+	(default, 2, 4, "2023-01-01", 4),
+	(default, 3, 3, "2022-12-03", 3),
+	(default, 4, 2, "2023-04-07", 1),
+	(default, 5, 1, "2023-05-21", 5),
+	(default, 5, 4, "2023-06-12", 6),
+	(default, 4, 4, "2022-07-03", 4),
+	(default, 3, 2, "2023-08-19", 6),
+	(default, 3, 1, "2021-09-25", 2),
+	(default, 1, 5, "2023-10-31", 7);
+
+SELECT * 
+FROM registros
+order by evaluacion desc
+limit 3;
+
+SELECT *
+FROM registros
+order by evaluacion
+limit 3;
+
+SELECT id_operario, nombre, apellido, COUNT(*) AS numero_operaciones FROM registros 
+JOIN operario ON operario.id = registros.id_operario
+group by id_operario
+order by numero_operaciones desc
+limit 1;
+
+SELECT id_usuario, nombre, apellido, COUNT(*) AS numero_consultas FROM registros
+JOIN usuario ON usuario.id = registros.id_usuario
+group by id_usuario
+order by numero_consultas asc
+limit 1;
